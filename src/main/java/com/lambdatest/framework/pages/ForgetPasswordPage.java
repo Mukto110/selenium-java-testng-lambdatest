bc9b8f82e534e3150f809e2b83072c03a5d842e2
Meraj Hossain Mukto <merajhossainmukto@gmail.com>
2025-09-28 13:15:33 +0600

    fixed some issue in login tests

--- src/main/java/com/lambdatest/framework/pages/ForgetPasswordPage.java
+++ src/main/java/com/lambdatest/framework/pages/ForgetPasswordPage.java
@@ -2,6 +2,8 @@ package com.lambdatest.framework.pages;
 
 import com.lambdatest.framework.base.BasePage;
 import com.lambdatest.framework.utils.ElementActions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class ForgetPasswordPage extends BasePage {
 
@@ -10,5 +12,12 @@ public class ForgetPasswordPage extends BasePage {
     public ForgetPasswordPage(ElementActions actions) {
         super();
         this.actions = new ElementActions(driver);
+    }
+
+    @FindBy(css = ".page-title.h3.mb-3")
+    private WebElement forgetPasswordPageHeader;
+
+    public String getForgetPasswordPageHeaderText() {
+        return actions.getText(forgetPasswordPageHeader);
     }
 }
